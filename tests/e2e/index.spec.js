76381096c42840f5295e0d221a0725e6846c6bad
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-13 21:10:19 +0100

    [JS-Test] Update tests files and fixtures

--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -1,5 +1,5 @@
 const { exec } = require("child_process");
-describe('Backend behaviour', () => {
+describe("Backend behaviour", () => {
 
     beforeAll(async () => {
         await runServer()
@@ -11,20 +11,31 @@ describe('Backend behaviour', () => {
         console.log("afterAll finish")
     })
 
-    test('a', async () => {
-        const response = { body: "message" };
-        const realResponse = await fetch('http://localhost:3000/api/v1/messages');
-        console.log(realResponse)
+    test("a", async () => {
+        const expectedResponse = "Hola, este es un mensaje de prueba cargado desde un fichero";
+        const response = (await (await fetch("http://localhost:3000/api/v1/messages?device=test_device1")).json()).message;
+        
+        expect(response).toStrictEqual(expectedResponse)
     });
 });
 
 let server;
 
-async function runServer (port) {
-    server = exec('node --run test:backend')
-
+async function runServer () {
+    server = exec("npm run test:e2e:backend");
+    return new Promise((res) => {
+        setTimeout(() => res(), 300);
+    })
+    
 }
 
-async function stopServer() {
-    setTimeout(() => server.kill(), 1000);
+
+
+function stopServer() {
+    return new Promise((res) => {
+        setTimeout(() => {
+            server.kill()
+            res()
+        }, 700);
+    })
 }
