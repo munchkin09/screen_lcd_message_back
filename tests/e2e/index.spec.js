77a103ee02dcdaed849afad4bb175df82f7f30a3
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-14 16:21:55 +0100

    [JS-Test] Add error handling test cases and fixtures

--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -2,8 +2,6 @@ const { exec } = require("child_process");
 describe("Backend behaviour", () => {
 
     beforeAll(async () => {
-        await runServer()
-        console.log("beforeAll finish")
     })
 
     afterAll(async () => {
@@ -13,22 +11,43 @@ describe("Backend behaviour", () => {
 
     test("Happy path, should work flawlessly", async () => {
         const expectedResponse = "Hola, este es un mensaje de prueba cargado desde un fichero";
+
+        await runServer("npm run test:e2e:backend")
+
         const response = (await (await fetch("http://localhost:3000/api/v1/messages?device=test_device1")).json()).message;
 
         expect(response).toStrictEqual(expectedResponse)
     });
 
-    test("Should no start when no info is given", async () => {
+    describe("Error handling", () => {
+        test("Should no start when no messages path is given", async () => {
+            const expectedError = "Messages path not valid, should be absolute path or relative to execution folder";
+            const executionOutput = await runServer("npm run test:e2e:backend-messages-no-path");
 
+            expect(executionOutput).toStrictEqual(expect.stringContaining(expectedError))
+
+        })
+
+        test("Should no start when no messages path is given", async () => {
+            const expectedError = "Messages path not valid, should be absolute path or relative to execution folder";
+            const executionOutput = await runServer("npm run test:e2e:backend-no-port");
+
+            expect(executionOutput).toStrictEqual(expect.stringContaining(expectedError))
+
+        })
     })
+    
 });
 
 let server;
 
-async function runServer () {
-    server = exec("npm run test:e2e:backend");
+async function runServer (command) {
+    let stdoutReturned;
+    server = exec(command, (error, stdout, stderr) => {
+        stdoutReturned = stdout
+    });
     return new Promise((res) => {
-        setTimeout(() => res(), 600);
+        setTimeout(() => res(stdoutReturned), 250);
     })
     
 }
