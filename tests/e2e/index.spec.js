ab64af8a097c404450f6806e17935368c53dca75
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-14 15:41:12 +0100

    [JS-Test] Small refactor on test cases add one more case(WIP)

--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -11,12 +11,16 @@ describe("Backend behaviour", () => {
         console.log("afterAll finish")
     })
 
-    test("a", async () => {
+    test("Happy path, should work flawlessly", async () => {
         const expectedResponse = "Hola, este es un mensaje de prueba cargado desde un fichero";
         const response = (await (await fetch("http://localhost:3000/api/v1/messages?device=test_device1")).json()).message;
-        
+
         expect(response).toStrictEqual(expectedResponse)
     });
+
+    test("Should no start when no info is given", async () => {
+
+    })
 });
 
 let server;
@@ -24,7 +28,7 @@ let server;
 async function runServer () {
     server = exec("npm run test:e2e:backend");
     return new Promise((res) => {
-        setTimeout(() => res(), 300);
+        setTimeout(() => res(), 600);
     })
     
 }
--- tests/messages.spec.js
+++ tests/messages.spec.js
@@ -1,10 +1,16 @@
-const { messagesController } = require('../src/controllers/messages')
+const { buildMessagesController } = require('../src/controllers/messages')
 
+const fakeLog = {
+    log: jest.fn(),
+    warn: jest.fn(),
+    info: jest.fn(),
+    error: jest.fn()
+}
 
 describe('Utests for messages controller logic', () => {
     test('Should load a list of messages', () => {
         const messagesFixtures = { test_1: "testing message set" }
-
+        const messagesController = buildMessagesController(fakeLog)
         messagesController.setMessages(messagesFixtures)
 
         expect(messagesController.getMessage("test_1")).toStrictEqual("testing message set")
