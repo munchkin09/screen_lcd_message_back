2e4316709ea025d24e9c6cf639ef142e59e6fcd4
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-15 22:26:31 +0100

    [JS] Change on main method with new repo logic

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const port = process.env.PORT;
 const apiKey = process.env.API_KEY;
-const messagesPath = process.env.MESSAGES;
+const dbPath = process.env.DB;
 const logName = 'backend_log.log'
 
+const buildRepositories = require('./models');
 const { buildMessagesController } = require('./controllers');
 const { startServer } = require('./server');
 const { buildLogger } = require('./ Logger');
 
 let logger;
-let messages = {};
 
 //Entry point for backend startup
 (async () => {
@@ -20,36 +20,25 @@ let messages = {};
         validateEnvironmentParams()
     } catch (error) {
         console.log("Something goes wrong on validation, error is: ", error)
+        return;
     }
 
     logger = await buildLogger('file', logName);
-    const messagesController = buildMessagesController(logger);
-    await setupMessages(messagesController);
+    const { devices, messages } = await buildRepositories(logger, dbPath);
+    const messagesController = buildMessagesController(logger, devices, messages);
+
     await startServer(port, apiKey, logger, messagesController);
 
-
     function validateEnvironmentParams() {
-        if (Number.isNaN(port)) {
+        console.log(isNaN(port));
+        if (isNaN(port) === true) {
             throw new Error("Port is mandatory");
         }
 
         try {
-            path.basename(messagesPath)
+            path.basename(dbPath)
         } catch(error) {
-            throw new Error("Messages path not valid, should be absolute path or relative to execution folder");
+            throw new Error("DB path is not valid, should be absolute path or relative to execution folder");
         }
     }
-
-    async function setupMessages(messagesController) {
-        const normalizedPath = path.resolve(cwd(), messagesPath);
-        const plainMessages = (await fs.readFile(normalizedPath, 'utf8')).split('\n');
-    
-        plainMessages.forEach(rawMessage => {
-            const [device, message] = rawMessage.split('|');
-            messages[device] = message;
-        })
-        messagesController.setMessages(messages);
-    
-    }
-
 })()
