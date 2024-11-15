7d9abcf91d2a7f8efce756e397c6104f430d2d51
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-15 22:26:05 +0100

    [JS] Add sql files with queries

---
+++ src/models/sql_statements/sql_messages.js
@@ -0,0 +1,13 @@
+module.exports = {
+    checkTable: "SELECT count(*) FROM sqlite_schema WHERE type='table' AND name='messages';",
+    createTable: `
+        CREATE TABLE messages(
+            key INTEGER PRIMARY KEY,
+            device TEXT,
+            message TEXT
+        ) STRICT
+    `,
+    createMessage: "INSERT INTO messages (key, message, device) VALUES (?, ?, ?)",
+    readMessages: "SELECT message FROM messages WHERE device = ? ",
+    deleteMessages: "DELETE FROM messages WHERE device = ?"
+}
