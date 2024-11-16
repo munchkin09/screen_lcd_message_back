3c43b0e41a64533c2a9f1e833753e4add01d412d
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-16 18:13:28 +0100

    [chore] Add express-validations for better data handling Add json parser middleware

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const { buildV1Routes }= require('./routes');
 
 module.exports = {
     startServer: async function(port, apiKey, logger, messagesController) {
-
+    app.use(express.json());
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
