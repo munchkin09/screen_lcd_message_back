fc44b30475e817f889012083660251ab9ca1ddad
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-16 18:14:53 +0100

    [JS-test] Add messages repository test case

--- tests/repositories/messages.spec.js
+++ tests/repositories/messages.spec.js
@@ -13,9 +13,14 @@ const fakeLog = {
 describe("Messages Repository SQL Lite implementation", () => {
     test("Should create a new DB with given data", async () => {
         //const messagesRepository = await buildMessagesRepository(fakeLog, path.resolve(process.cwd(), 'test/fixtures/db-msg-repo.sql'), ['uno','dos','tres']);
+        const dbPath = "tests/fixtures/repositories/empty-db.db"
+        const database = new sqlite.DatabaseSync(dbPath);
+
+        await initializeMessagesDb(fakeLog, database);
+    });
+
+    test("Should build a messages repo and return a valid interface", () => {
         const dbPath = "tests/fixtures/db.db"
         const database = new sqlite.DatabaseSync(dbPath);
-        console.log(database);
-        await initializeMessagesDb(fakeLog, database);
     });
 })
