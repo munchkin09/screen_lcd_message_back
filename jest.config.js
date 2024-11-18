3d5afec03f7f0084e97148dca7be6d66928c1e5b
CarlosL <carlos.ledesma1983@gmail.com>
2024-11-18 14:27:48 +0100

    [JS-test] Add better reporting Add dev dependencies Add CI github actions config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
+    collectCoverage: true,
+    reporters: [
+        'summary',
+        'jest-silent-reporter',
+        ['github-actions', {silent: false}],
+        ['jest-junit', {outputDirectory: 'test-results'}]
+    ],
+    coverageReporters: ["json", "text"]
 };
 
 module.exports = config;
