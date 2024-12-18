7218f3ff8b99fa86d6a13e2f5fe772251349d7c8
CarlosL <carlos.ledesma1983@gmail.com>
2024-12-18 13:51:23 +0100

    [chore] more eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,19 @@
-export default [
+const pluginJest = require('eslint-plugin-jest');
+
+module.exports = [
     {
-        files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+        // update this to match your test files
+        files: ['**/*.spec.js', '**/*.test.js'],
+        plugins: { jest: pluginJest },
+        languageOptions: {
+            globals: pluginJest.environments.globals.globals,
+        },
         rules: {
-            "prefer-const": "warn",
-            "no-constant-binary-expression": "error"
-        }
-    }
+            'jest/no-disabled-tests': 'warn',
+            'jest/no-focused-tests': 'error',
+            'jest/no-identical-title': 'error',
+            'jest/prefer-to-have-length': 'warn',
+            'jest/valid-expect': 'error',
+        },
+    },
 ];
